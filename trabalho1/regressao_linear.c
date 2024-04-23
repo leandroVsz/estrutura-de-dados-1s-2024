bfeeb567256aac474ca9c1bba3c2b69c23399387
leandroVsz <leandroverasdesouza@gmail.com>
2024-04-23 10:50:14 -0300

    regressão

--- trabalho1/regressao_linear.c
+++ trabalho1/regressao_linear.c
@@ -5,23 +5,24 @@
 #define MAXCHAR 1000
 
 int main(int argc, char* argv[]){
+    typedef struct Ponto{
+        int valorx;
+        float valory;
+    } Ponto;
+    
     FILE *arquivo_entrada = fopen(argv[1], "r");
     if (arquivo_entrada == NULL) {
         printf("Erro ao abrir o arquivo %s\n", argv[1]);
         return 1;
     }
 
-    typedef struct Ponto{
-        int valorx;
-        float valory;
-    } Ponto;
+    
 
-    char i[MAXCHAR];
+    Ponto *lista_pontos;
     int num_numeros = 0;
     while (fscanf(arquivo_entrada, "%s", &i) != EOF) {
         num_numeros++;
     }
-    printf("vasco");
     printf("%s", i);
     
     fclose(arquivo_entrada);
