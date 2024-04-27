b5e508ad09aba00141d2aa1c254b01fbaa8d1522
leandroVsz <leandroverasdesouza@gmail.com>
2024-04-27 09:43:41 -0300

    atividade4

--- Lista1/atividade4/lista.c
+++ Lista1/atividade4/lista.c
@@ -47,3 +47,96 @@ void liberar(No* H){
         free(H);
     }
 }
+
+int verificar_existencia(No* H, char valor_busca){
+    if(H != NULL){
+        if(H->valor == valor_busca){
+            return 1;
+        }
+        return verificar_existencia(H->proximo_no, valor_busca);
+    }
+    return 0;
+}
+
+int verificar_ocorrencias(No* H, char valor_busca){
+    if(H != NULL){
+        if(H->valor == valor_busca){
+            return 1 + verificar_ocorrencias(H->proximo_no, valor_busca);
+        }
+        return 0 + verificar_ocorrencias(H->proximo_no, valor_busca);
+    }
+    return 0;
+}
+
+void imprimir_inversa(No* H){
+    if(H != NULL){
+        imprimir_inversa(H->proximo_no);
+        printf("%c ", H->valor);
+    }
+}
+
+
+void lista_inserir_no_i(No** H, int i, char valor) {
+    No* novo_no = (No*)malloc(sizeof(No));
+    novo_no->valor = valor;
+    novo_no->proximo_no = NULL;
+
+    if(i == 0) {
+        novo_no->proximo_no = *H;
+        *H = novo_no;
+    } 
+    else {
+        No* temp = *H;
+        for(int j = 0; j < i-1 && temp != NULL; j++)
+            temp = temp->proximo_no;
+        if(temp == NULL)
+            return;
+        novo_no->proximo_no = temp->proximo_no;
+        temp->proximo_no = novo_no;
+    }
+}
+
+void lista_remover_no_i(No** H, int i) {
+    if(*H == NULL)
+        return;
+
+    No* temp = *H;
+
+    if(i == 0) {
+        *H = temp->proximo_no;
+        free(temp);
+        return;
+    }
+
+    for(int j = 0; temp != NULL && j < i-1; j++)
+        temp = temp->proximo_no;
+
+    if(temp == NULL || temp->proximo_no == NULL)
+        return;
+
+    No* proximo_no = temp->proximo_no->proximo_no;
+    free(temp->proximo_no);
+    temp->proximo_no = proximo_no;
+}
+
+void lista_remover_no(No** H, char valor_busca) {
+    while(*H != NULL && (*H)->valor == valor_busca) {
+        No* temp = *H;
+        *H = (*H)->proximo_no;
+        free(temp);
+    }
+
+    if(*H == NULL)
+        return;
+
+    No* temp = *H;
+    while(temp->proximo_no != NULL) {
+        if(temp->proximo_no->valor == valor_busca) {
+            No* no_a_remover = temp->proximo_no;
+            temp->proximo_no = temp->proximo_no->proximo_no;
+            free(no_a_remover);
+        } else {
+            temp = temp->proximo_no;
+        }
+    }
+}
--- Lista1/atividade4/main.c
+++ Lista1/atividade4/main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[]){
 
-    No* pilha1 = no('A', no('B', no('C', no('D', no('E', no('F', NULL))))));
+    No* pilha1 = no('A', no('B', no('C', no('D', no('B', no('F', NULL))))));
     No* pilha_copia = copiar(pilha1);
     printf("Pilha original: ");
     imprimir(pilha1);
@@ -16,8 +16,31 @@ int main(int argc, char* argv[]){
     
     
     printf("\nqtd = %d", qnt_nos(pilha1));
-    printf("\ncopia qtd = %d", qnt_nos(pilha_copia));
+    printf("\ncopia qtd = %d\n", qnt_nos(pilha_copia));
     
+    printf("\nValor 'C' esta presente? %d", verificar_existencia(pilha1, 'C'));
+    printf("\nValor 'Z' esta presente? %d\n", verificar_existencia(pilha1, 'Z'));
+
+    printf("\nOcorrencias da letra 'C': %d", verificar_ocorrencias(pilha1, 'C'));
+    printf("\nOcorrencias da letra 'B' %d\n", verificar_ocorrencias(pilha1, 'B'));
+
+    printf("\nPilha inversa: \n");
+    imprimir_inversa(pilha1);
+ 
+    lista_inserir_no_i(&pilha1, 0, 'Z');
+    printf("\n\n");
+    imprimir(pilha1);
+
+    lista_remover_no_i(&pilha1, 4);
+    printf("\n\n");
+    imprimir(pilha1);
+
+
+    lista_remover_no(&pilha1, 'B');
+    printf("\n\n");
+    imprimir(pilha1);
+
+
     liberar(pilha1);
     liberar(pilha_copia);
 
